df410ab7d819bc1a2c4e5a392e0a42e1b3fd2bbe
italoalmeida <it4lo.almeida@gmail.com>
2021-04-25 21:30:09 -0300

    chore

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,6 +2,9 @@ import  setupRoutes  from "@/main/config/router-express-config";
 
 import express from "express";
 const app = express();
+
+app.use(express.json());
+
 setupRoutes(app);
 
 export default app;
